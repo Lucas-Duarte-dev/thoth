9672c70b06faa0c86d7c26d29fa8ce6572b1dd0e
lucas <lucas.goes@webjump.com.br>
2023-04-10 16:47:02 -0300

    feat: created connection with redis

--- src/infra/http/index.ts
+++ src/infra/http/index.ts
@@ -2,6 +2,6 @@ import { app } from './server'
 import dotenv from 'dotenv'
 
 dotenv.config()
-app.listen(process.env.PORT || 3000)
-
-console.log(`Server running in http://localhost:${process.env.PORT || 3000}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running in http://localhost:${process.env.PORT || 3000}`)
+})
